06aa44620a2203b6ba3ba04349576746977341a1
Isak <isak.shamun@gmail.com>
2023-05-22 16:12:07 +0200

    update testing

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,21 +1,33 @@
 import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-const increaseByOne = () =>setCounter(counter+1)
-const setToZero = () => setCounter(0)
+
+const Display = ({counter}) => <div>{counter}</div>
+const Button = ({handleClick, text}) => <button onClick ={handleClick}>{text}</button>
+
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+}
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+}
 
   return (
-  <div>
-    <div>{counter}</div>
-    <button onClick={increaseByOne}>
-      plus
-    </button>
-    <button onClick={setToZero}> 
-      zero
-    </button>
-  </div>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
   )
 }
 
