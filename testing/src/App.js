a62cf943e96f9557855ccc5e1cca81e4b6ffbc84
Isak <isak.shamun@gmail.com>
2023-07-14 10:14:00 +0200

    update of testing

--- testing/src/App.js
+++ testing/src/App.js
@@ -3,30 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 const Display = ({counter}) => <div>{counter}</div>
-const Button = ({handleClick, text}) => <button onClick ={handleClick}>{text}</button>
+const Button = ({handleClick, text}) =>
+ <button onClick ={handleClick}>
+    {text}
+  </button>
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left+1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft+right)
 }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right+1)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight+left)
 }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+      <History allClicks={allClicks}/>
     </div>
   )
 }
