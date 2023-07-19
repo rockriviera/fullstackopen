a5297052ccbc1f56e5c7b8ba7d3178b06c9f791f
Isak <isak.shamun@gmail.com>
2023-07-19 16:29:27 +0200

    2.8 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
-
+  const [newNumber, setNewNumber] = useState('')
   const Person =(props) =>{
     return(
-      <li>{props.name}</li>
+      <li>{props.name} {props.number}</li>
     )
   }
 
@@ -26,8 +29,13 @@ const App = () => {
       alert(`${newName} already exists in Phonebook`)
       return
     }
+    if((newNumber.length<1)||(newName.length <1) ){
+      alert (`Both fields required`)
+      return
+    }
     const nameObject = {
       name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(nameObject))
@@ -36,6 +44,10 @@ const App = () => {
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -50,13 +62,20 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input 
+          value={newNumber}
+          onChange ={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         <ul>
           {persons.map(person=>
-            <Person key={person.name} name={person.name}/>)}
+            <Person key={person.name} name={person.name} number={person.number}/>)}
         </ul>
     </div>
   )
